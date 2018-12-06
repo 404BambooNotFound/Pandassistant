5a66c7ae89e3e98fb3c058ea8c2868f08f1f1b69
Théo Bertet <38724527+TheoBertet@users.noreply.github.com>
2018-12-06 22:56:36 +0100

    Correct Bootstrap + Navbar

--- index.php
+++ index.php
@@ -14,37 +14,57 @@ include 'PHP/connectDB.php';
     <head>
         <title><?php echo $websiteName ?> - About</title>
         <meta charset="utf-8"/>
-        <link rel="stylesheet" href="css/bootstrap-grid.min.css"/>
-        <link rel="stylesheet" href="css/bootstrap-reboot.min.css"/>
-        <link rel="stylesheet" href="css/bamboostyle.css"/>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/bootstrap-grid.min.css" />
+        <link rel="stylesheet" href="css/bootstrap-reboot.min.css" />
+        <link rel="stylesheet" href="css/bamboostyle.css" />
         <script src="js/jquery-3.3.1.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/bootstrap.bundle.min.js"></script>
     </head>
 
     <body>
-        <div class="container">
+<!--        <div class="container">-->
             <div class="row">
-                <img class="img-fluid" src="img/brucebanner.png"/>
-            </div>
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a class="navbar-brand" href="#">Navbar</a>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" href="#">Disabled</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+<!--            </div>-->
 
             <div class="row">
                 <div class="col-md-2"></div>
 
                 <div class="col-md-8 contenu">
 
-                    <h1>Team 404 - Bamboo Not Found</h1>
-<!--                    <h2>...Site en construction...</h2>-->
-                    <?php
-                        $req = $db->prepare("SELECT * FROM teammate");
-                        $req->execute();
-                        $res = $req->fetchAll();
-                        echo "<ul>";
-                        foreach($res as $line) {
-                                echo "<li>" . $line['firstname'] . " " . $line['lastname'] . "</li>";
-                        }
-                        echo "</ul>";
-                    ?>
+
+
 
                 </div>
 
