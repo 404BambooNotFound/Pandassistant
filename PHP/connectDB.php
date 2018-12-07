81afa66792c5796dbec2605b7647c1cff11419b0
Je5ter <yann.galan@wanadoo.fr>
2018-12-07 05:19:13 +0100

    Fix

--- PHP/connectDB.php
+++ PHP/connectDB.php
@@ -7,7 +7,7 @@
  */
 
 try {
-    $db = new PDO('mysql:host=localhost:3306;dbname=pandassistant', 'admin', 'admin');
+    $db = new PDO('mysql:host=localhost:3307;dbname=pandassistant', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo $e->getMessage();
